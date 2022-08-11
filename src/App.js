28dc93f48be19643c3847a121ada0677b4e8d5bf
Collins Adhola <coadhola@gmail.com>
2022-08-11 11:46:28 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+
+import Expenses from "./components/Expenses";
+import Nav from "./components/Nav";
+
+
+function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Paper Cups",
+      amount: 700,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "Ice", amount: 400, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Mangos",
+      amount: 500,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "Lunch",
+      amount: 1000,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Nav />
+      <Expenses item={expenses}/>
+      
+
+     
+    </div>
+  );
+}
+
+export default App;

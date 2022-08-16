5eb17871aa26e094be7c54f320b517ab4b46c9f9
Collins Adhola <coadhola@gmail.com>
2022-08-16 17:23:35 +0100

    year filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,6 +10,25 @@ function Expenses(props) {
     setFilteredYear(selectedYear);
   };
 
+ const filteredExpenses = props.item.filter((expense) => {
+  return expense.date.getFullYear().toString() === filteredYear;
+ });
+
+
+ let expensesContent = <p>No expenses found.</p>;
+
+ if(filteredExpenses.length > 0){
+  expensesContent = filteredExpenses.map((expense) => (
+     <ExpenseItem
+       key={expense.id}
+       title={expense.title}
+       amount={expense.amount}
+       date={expense.date}
+     />
+
+  ));
+ }
+
   return (
     <div>
       <Card className="expenses">
@@ -18,14 +37,11 @@ function Expenses(props) {
           onChangeFilter={filterChangeHandler}
         />
      
-        {props.item.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+       
+      
+
+         {expensesContent}
+      
       </Card>
     </div>
   );
